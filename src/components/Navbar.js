7deabbce3e24e79dfa9fca974799b65971033049
manami-d <manami.davidson@hotmail.com>
2021-03-02 16:50:23 +1100

    Add font family and resize the top image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,46 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import styled from 'styled-components'
+
+// const Button = styled.button`
+//   background: transparent;
+//   border-radius: 3px;
+//   border: 2px solid palevioletred;
+//   color: palevioletred;
+//   margin: 0 1em;
+//   padding: 0.25em 1em;
+
+//   ${props =>
+//     props.primary &&
+//     css`
+//       background: palevioletred;
+//       color: white;
+//     `};
+// `
+const Ul = styled.ul`
+  list-style-type: none;
+  padding: 0; 
+  margin: 0; 
+`
 
 const Navbar = () => {
   return (
+
     <div>
-      <ul>
+      <Ul>
         <li>
+          {/* <Button>Home</Button> */}
           <Link to="/">Home</Link>
         </li>
         <li>
+          {/* <Button promary>Projects</Button> */}
+
           <Link to="/projects">Projects</Link>
         </li>
         <li>
           <Link to="/contact">Contact</Link>
         </li>
-      </ul>
+      </Ul>
       
     </div>
   )
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const GlobalStyles = styled.main`
+  font-family: "Cardo", Helvetica;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+`
