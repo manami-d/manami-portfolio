afc337874852fbfcada38b45d7862e9363fb1f39
manami-d <manami.davidson@hotmail.com>
2021-03-10 11:17:51 +1100

    Fix position of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className="footer">
             <p>Copyright &copy; 2021 Manami Davidson</p>
         </div>
     );
