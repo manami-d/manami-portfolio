68baaf82bfddba1590a2c529fd3ffcbfe54e4a5b
manami-d <manami.davidson@hotmail.com>
2021-03-10 21:25:13 +1100

    Add project images and need to fix positioning

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,23 +1,66 @@
 import React from 'react';
+import portfolioImg from '../img/portfolio.png';
+import todoappImg from '../img/todoapp.png';
+import recipewebsiteImg from '../img/recipewebsite.png';
+import weatherappImg from '../img/weatherApp.png';
+
+import { Card } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+
 // import ProjectLinks from '../components/ProjectLinks';
 
 const Projects = () => {
     return (
         <div>
             <h1>Projects</h1>
-            <br />
-            <br />
-            <a href="https://jwd4-todolist-app.netlify.app/">To Do List App</a>
-            <p>HTML|CSS|JavaScript|Bootstrap</p>
-            <br />
-            <a href="https://recipe-website-bootstrap.netlify.app/">Recipe Website</a>
-            <p>HTML|CSS|Bootstrap</p>
-            <br />
-            <a href="https://github.com/manami-d/Weather-API">Weather App</a>
-            <p>HTML|CSS|JavaScript|Bootstrap|APIs</p>
-            <br />
-            <a href="https://manami-davidson.netlify.app/">Portfolio Website</a>
-            <p>HTML|CSS|JavaScript|React</p>
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" style={{ width: '10vw' }} src={todoappImg} />
+                            <Card.Body>
+                                <Card.Title>To Do App</Card.Title>
+                                <Card.Text>HTML|CSS|JavaScript|Bootstrap</Card.Text>
+                                <Card.Link href="https://jwd4-todolist-app.netlify.app/">Check out the app</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" style={{ width: '10vw' }} src={recipewebsiteImg} />
+                            <Card.Body>
+                                <Card.Title>Recipe Website</Card.Title>
+                                <Card.Text>HTML|CSS|JavaScript|React|React Bootstrap</Card.Text>
+                                <Card.Link href="https://recipe-website-bootstrap.netlify.app/">Check out the app</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" style={{ width: '10vw' }} src={weatherappImg} />
+                            <Card.Body>
+                                <Card.Title>Weather App</Card.Title>
+                                <Card.Text>HTML|CSS|JavaScript|Bootstrap|APIs</Card.Text>
+                                <Card.Link href="https://github.com/manami-d/Weather-API">Check out the app</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" style={{ width: '10vw' }} src={portfolioImg} />
+                            <Card.Body>
+                                <Card.Title>Portfolio Website</Card.Title>
+                                <Card.Text>HTML|CSS|JavaScript|React|React Bootstrap</Card.Text>
+                                <Card.Link href="https://manami-davidson.netlify.app/">Check out the app</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
