19745eaaa0a70da0c5b9e1e33835638e1197ec4f
manami-d <manami.davidson@hotmail.com>
2021-04-18 10:06:21 +1000

    change the size of the top image for mobile and tablet

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,22 +14,40 @@ const StyledAbout = styled(styledPage)`
     p {
         margin: 10px 0;
     }
+
+    img {
+        margin-top: 10px;
+        width: 60vw;
+    }
+
+    //tablet
+    @media only screen and (min-width: 720px) {
+        img {
+            width: 50vw;
+        }
+    }
+    //desktop
+    @media only screen and (min-width: 1200px) {
+        img {
+            width: 30vw;
+        }
+    }
 `;
 
 const About = () => {
-    const topImageStyle = {
-        width: '30vw'
-    };
+    // const topImageStyle = {
+    //     width: '30vw'
+    // };
 
     return (
         <StyledAbout>
             <div className={'main-left'}>
                 <h1>Manami Davidson</h1>
                 <h3>Web Developer</h3>
-                <p>HTML | CSS | JavaScript | Bootstrap | Git | Github</p>
+                <p>HTML | CSS | JavaScript | Bootstrap | Git | Github | WordPress | SEO</p>
             </div>
             <div className={'main-right'}>
-                <img src={topImage} alt="beach and combi" style={topImageStyle} />
+                <img src={topImage} alt="beach and combi" />
             </div>
         </StyledAbout>
     );
